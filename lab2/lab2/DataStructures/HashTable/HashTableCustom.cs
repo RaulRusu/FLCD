ab22bdc1132e480872537b68340f46b11193f29c
Raul Rusu <raul.rusu@tractorsetgo.ro>
2020-10-16 14:11:34 +0300

    feat: Add symbol table internally represented as a hash table feat: Add data structureas hash table and linked list

---
+++ lab2/lab2/DataStructures/HashTable/HashTableCustom.cs
@@ -0,0 +1,78 @@
+﻿using lab2.DataStructures.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace lab2.DataStructures.HashTable
+{
+    public class HashTableCustom
+    {
+        const int DEFAULT_BUCKET_SIZE = 1000;
+        private LinkedListCustom[] buckets;
+        public int Size { private set; get; }
+
+        public HashTableCustom()
+        {
+            this.Size = DEFAULT_BUCKET_SIZE;
+            buckets = new LinkedListCustom[DEFAULT_BUCKET_SIZE];
+        }
+
+        public HashTableCustom(int size)
+        {
+            this.Size = size;
+            buckets = new LinkedListCustom[size];
+        }
+
+        /// <summary>
+        /// Computes the hash of a string using djb2 Algorithm (simple to implement, works good in practice)
+        /// </summary>
+        /// <param name="value"></param>
+        private int hashFunction(string value)
+        {
+            var hash = 5381;
+
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(value);
+            asciiBytes.ToList().ForEach(asciiByte => hash = (hash * 33 + asciiByte) % Size);
+
+            return hash;
+        }
+
+        /// <summary>
+        /// Add an element in the hash table
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>The position of the elemnt</returns>
+        public HashTablePosition Add(string value)
+        {
+            var hash = hashFunction(value);
+
+            var bucket = buckets[hash];
+            if (buckets[hash] == null)
+                bucket = new LinkedListCustom();
+            bucket.AddLast(value);
+
+            return new HashTablePosition(hash, bucket.Size - 1);
+        }
+
+        /// <summary>
+        /// Find an element in the hash table by it's value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>The position in the hash table or null if it doesn't exists</returns>
+        public HashTablePosition Find(string value)
+        {
+            var hash = hashFunction(value);
+
+            var bucket = buckets[hash];
+            if (bucket == null)
+                return null;
+
+            var index = bucket.GetIndex(value);
+            if (!index.HasValue)
+                return null;
+
+            return new HashTablePosition(hash, index.Value);
+        }
+    }
+}
---
+++ lab2/lab2/DataStructures/LinkedList/LinkedListCustom.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2.DataStructures.LinkedList
+{
+    public class LinkedListCustom
+    {
+        public LinkedListNodeCustom First { get; set; } = null;
+        public LinkedListNodeCustom Last { get; set; } = null;
+        public int Size { get; private set; } = 0;
+
+        public LinkedListNodeCustom AddLast(string value)
+        {
+            var node = new LinkedListNodeCustom(value);
+
+            if (First == null)
+            {
+                this.First = node;
+                this.Last = node;
+            }
+            else
+            {
+                this.Last.Next = node;
+                this.Last = node;
+            }
+
+            Size++;
+            return node;
+        }
+
+        public int? GetIndex(string value)
+        {
+            var currentNode = this.First;
+            var index = 0;
+
+            while(currentNode != null)
+            {
+                if (currentNode.Value == value)
+                    return index;
+
+                index++;
+                currentNode = currentNode.Next;
+            }
+
+            return null;
+        }
+    }
+}
